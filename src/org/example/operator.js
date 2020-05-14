acd4e76e65f0e154c217d7e85880308ca58e2e17
vural <vural.dincer@tengen.com.tr>
2020-05-14 08:46:45 +0300

    Test Edilebilir ilk Sürüm

--- src/org/example/operator.js
+++ src/org/example/operator.js
@@ -1,36 +1,62 @@
+/**
+ * 
+ */
 import Exception from './exception';
 
+export default class Operator {
 
-export default class Operator { 
-    /*
-    const PLUS = '+';
-    const MINUS = '-';    
-    const MULTIPLE = '*';
-    const DIVIDE = '/';
-    */
 
-    constructor(){
+    constructor() {
 
     }
 
-    add(num1,num2) {
-        return (num1 + num2);
+    static get ADD() {
+        return '+';
     }
 
-    minus(num1,num2) { 
-        return (num1-num2);
+    static get MINUS() {
+        return '-';
     }
 
-    multiple(num1, num2) {
-        return (num1*num2);
+    static get MULTIPLE() {
+        return '*';
     }
 
-    divide(num1, num2) {
-        if( 0 == num2 ) {
+    static get DIVIDE() {
+        return '/';
+    }
+
+    static get SQR() {
+        return '^2';
+    }
+
+    add(num1, num2) {        
+        return ((num1) + (num2));
+    }
+
+    minus(num1, num2) {        
+        if( num1<0 && num2<0 ){
+            return ((num1) + (num2));
+        }else if( num2<0 ){
+            return ((num1) + (-1 * num2));
+        }else{
+            return ((num1) - (num2));
+        }
+    }
+
+    multiple(num1, num2) {        
+        return ((num1) * (num2));
+    }
+
+    divide(num1, num2) {        
+        if (0 == num2) {
             throw new Exception('Division By Zero');
         }
-        return (num1/num2);
+        return ((num1) / (num2));
     }
 
-    
+    sqr(num1){
+        return Math.pow(num1,2);
+    }
+
 }
--- src/org/example/simple.js
+++ src/org/example/simple.js
@@ -1,22 +1,52 @@
+/**
+ * 
+ */
 import Calculator from './calculator';
 import Operator from './operator';
 
+/**
+ * Simple Operation
+ */
 export default class Simple extends Calculator {
     /*private*/
     operator = {};
     result = 0.0;
 
-    constructor(){       
+
+    /**
+     * 
+     */
+    constructor() {
         super();
         this.operator = new Operator();
     }
 
-    operate(op, firts, second){
-        super.operate();
-        
+    /**
+     * 
+     * @param {*} op 
+     * @param {*} first 
+     * @param {*} second 
+     */
+    operate(op, first, second=0) {
+        super.operate(op, first, second);
+        switch (op) {
+            case '+':
+                this.result = this.operator.add(first, second);
+                break;
+            case '-':
+                this.result = this.operator.minus(first, second);
+                break;
+            case '*':
+                this.result = this.operator.multiple(first, second);
+                break;
+            case '/':
+                this.result = this.operator.divide(first, second);
+                break;       
+            case '^2':
+                this.result = this.operator.sqr(first)
+
+        }
+        return this.result;
     }
-
-
-
 
 }
