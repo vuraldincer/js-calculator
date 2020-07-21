73c493a1649748d667209ff6e1b73e2d17e093cd
vural <vural.dincer@tengen.com.tr>
2020-07-21 13:34:49 +0300

    fix: webpackage-merge const

--- build_config/webpack.prod.js
+++ build_config/webpack.prod.js
@@ -1,6 +1,5 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const path = require('path');
-
 const prod = require('./prod');
 
 module.exports = merge(prod, {});
